a8014b6f7ce8d33f8af8d58cf4ff08b18b4f9a26
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-10-23 02:01:26 +0530

    created tables

--- nutrient_management_system/about_us/models.py
+++ nutrient_management_system/about_us/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class AboutUs(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
--- nutrient_management_system/articles/models.py
+++ nutrient_management_system/articles/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=75)
+    content = models.CharField(max_length=1000)
+    likes = models.IntegerField(default=0)
+    publisher = models.CharField(max_length=50)
+    date = models.DateField()
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=500)
