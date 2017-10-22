200c0132ef39cead557511e033ea8b8f1856d1b9
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-10-23 02:34:49 +0530

    added tables to nutrients_info

--- nutrient_management_system/nutrients_info/admin.py
+++ nutrient_management_system/nutrients_info/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Food, Nutrient, Disease, Geographical, People, Seasonal
 
-# Register your models here.
+admin.site.register(Food)
+admin.site.register(Nutrient)
+admin.site.register(Disease)
+admin.site.register(Geographical)
+admin.site.register(People)
+admin.site.register(Seasonal)
--- nutrient_management_system/nutrients_info/models.py
+++ nutrient_management_system/nutrients_info/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Nutrient(models.Model):
+    name = models.CharField(max_length=100)
+    avg_intake = models.CharField(max_length=1000)
+
+
+class Disease(models.Model):
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    lack = models.BooleanField(default=False)
+    surplus = models.BooleanField(default=False)
+
+
+class People(models.Model):
+    people_types = (('athelete', 'athelete'), ('normal', 'normal'), ('pregnant-lady', 'pregnant-lady'))
+
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=people_types, default='athelete')
+
+
+class Food(models.Model):
+    nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+class Geographical(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+
+class Seasonal(models.Model):
+    food = models.ForeignKey(Food, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
