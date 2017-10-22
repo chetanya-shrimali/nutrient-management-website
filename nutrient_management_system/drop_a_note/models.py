6dd00df7df66f0e06027f39dec921efae9c3fdad
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-10-23 02:10:07 +0530

    added tables for drop a note

--- nutrient_management_system/drop_a_note/models.py
+++ nutrient_management_system/drop_a_note/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Note(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.CharField(max_length=100)
+    note = models.CharField(max_length=1000)
