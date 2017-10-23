7c8ce9b450cf6813560b79aaf29cbd69270b4059
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-10-23 14:58:55 +0530

    added models for likes

--- nutrient_management_system/articles/models.py
+++ nutrient_management_system/articles/models.py
@@ -5,11 +5,17 @@ class Article(models.Model):
     user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=75)
     content = models.CharField(max_length=1000)
-    likes = models.IntegerField(default=0)
     publisher = models.CharField(max_length=50)
     date = models.DateField()
 
 
 class Comment(models.Model):
     article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
     comment = models.CharField(max_length=500)
+
+
+class Like(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
