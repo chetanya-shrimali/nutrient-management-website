dee2fab0d9c342a9794f2a74286c97d6e9eb8a2a
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-07 01:48:13 +0530

    fixed article model

--- nutrient_management_system/drop_a_note/models.py
+++ nutrient_management_system/drop_a_note/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Note(models.Model):
     name = models.CharField(max_length=50)
     email = models.CharField(max_length=100)
-    note = models.TextField
+    note = models.TextField()
 
     def __str__(self):
         return self.name + ' -> ' + self.email
