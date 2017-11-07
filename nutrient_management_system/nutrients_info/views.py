cbd1f8349ddcb2912292c29789a4d5b09e659868
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-08 02:19:45 +0530

    added queries to templates

--- nutrient_management_system/nutrients_info/views.py
+++ nutrient_management_system/nutrients_info/views.py
@@ -7,13 +7,13 @@ def info(request):
     nutrients = Nutrient.objects.all()
     diseases = Disease.objects.all()
     foods = Food.objects.all()
-    seasonal = Seasonal.objects.all()
-    people = People.objects.all()
-    geographical = Geographical.objects.all()
+    seasonals = Seasonal.objects.all()
+    peoples = People.objects.all()
+    geographicals = Geographical.objects.all()
     return render(request, 'nutrients_info.html', {'nutrients': nutrients,
                                                    'diseases': diseases,
                                                    'foods': foods,
-                                                   'seasonal': seasonal,
-                                                   'people': people,
-                                                   'geographical': geographical
+                                                   'seasonals': seasonals,
+                                                   'peoples': peoples,
+                                                   'geographicals': geographicals
                                                    })
