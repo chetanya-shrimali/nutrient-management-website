30ea94a1f1a2601e22e6975d876cba18f1bbbc47
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-08 00:26:40 +0530

    added and altered templates

--- nutrient_management_system/nutrients_info/views.py
+++ nutrient_management_system/nutrients_info/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Disease, Food, Geographical, Nutrient, People, Seasonal
 
 
 def info(request):
-    return render(request, 'services.html')
+    nutrients = Nutrient.objects.all()
+    return render(request, 'nutrients_info.html', {'nutrients': nutrients})
