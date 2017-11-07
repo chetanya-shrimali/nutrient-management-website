8adda2c2a99d16dcd699b7c71a75ed11e1706f28
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-08 00:38:41 +0530

    added `__str__` functions

--- nutrient_management_system/nutrients_info/models.py
+++ nutrient_management_system/nutrients_info/models.py
@@ -5,12 +5,18 @@ class Nutrient(models.Model):
     name = models.CharField(max_length=100)
     avg_intake = models.CharField(max_length=1000)
 
+    def __str__(self):
+        return self.name
+
 
 class Disease(models.Model):
     nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     lack = models.BooleanField(default=False)
     surplus = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nutrient.name + ' -> ' + self.name
 
 
 class People(models.Model):
@@ -21,17 +27,29 @@ class People(models.Model):
     type = models.CharField(max_length=20, choices=people_types,
                             default='athelete')
 
+    def __str__(self):
+        return self.nutrient.name + ' -> ' + self.type
+
 
 class Food(models.Model):
     nutrient = models.ForeignKey(Nutrient, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nutrient.name + ' -> ' + self.name
 
 
 class Geographical(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 
 class Seasonal(models.Model):
     food = models.ForeignKey(Food, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
