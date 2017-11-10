2081af143472e30d5e3c32448eba3b51abab4c03
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-11-10 09:54:11 +0530

    added database
    Signed-off-by: chetanya-shrimali <chetanyashrimalie5@gmailcom>

--- nutrient_management_system/home/views.py
+++ nutrient_management_system/home/views.py
@@ -1,11 +1,15 @@
+from django.apps import apps
 from django.shortcuts import render
 from django.views import View
-
 from home.forms import UserForm
 
 
 def home(request):
-    return render(request, 'index.html')
+    article_model = apps.get_model('articles.Article')
+    print(article_model)
+    articles = article_model.objects.all().order_by('-id')[:3]
+    print(articles)
+    return render(request, 'index.html', {'articles': articles})
 
 
 class UserFormView(View):
