efe11c39d3c20b175373fb1007d1ded9269d631b
chetanya-shrimali <chetanyashrimalie5@gmail.com>
2017-11-12 22:53:14 +0530

    secured api endpoints
    Signed-off-by: chetanya-shrimali <chetanyashrimalie5@gmail.com>

--- nutrient_management_system/nutrients_info/urls.py
+++ nutrient_management_system/nutrients_info/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required
 
 from nutrients_info import views
 
@@ -6,9 +7,14 @@ app_name = 'nutrients_info'
 
 urlpatterns = [
     url(r'^$', views.info, name='info'),
-    url(r'^api/season/$', views.SeasonAPI.as_view(), name='season_api'),
-    url(r'^api/food/$', views.FoodAPI.as_view(), name='food_api'),
-    # url(r'^api/nutrient/$', views.NutrientAPI.as_view(), name='nutrient_api'),
-    url(r'^api/people/$', views.PeopleAPI.as_view(), name='people_api'),
-    url(r'^api/disease/$', views.DiseaseAPI.as_view(), name='disease_api'),
+    url(r'^api/season/$', login_required(views.SeasonAPI.as_view()),
+        name='season_api'),
+    url(r'^api/food/$', login_required(views.FoodAPI.as_view()),
+        name='food_api'),
+    # url(r'^api/nutrient/$', views.NutrientAPI.as_view(),
+    # name='nutrient_api'),
+    url(r'^api/people/$', login_required(views.PeopleAPI.as_view()),
+        name='people_api'),
+    url(r'^api/disease/$', login_required(views.DiseaseAPI.as_view()),
+        name='disease_api'),
 ]
