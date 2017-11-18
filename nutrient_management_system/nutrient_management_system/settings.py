ea33befab2ed76bcf0b5aee7f5b8a9fd154fee33
chetanya-shrimali <chetanyashrimalie5@gmailcom>
2017-11-19 00:37:39 +0530

    updated settings
    Signed-off-by: chetanya-shrimali <chetanyashrimalie5@gmailcom>

--- nutrient_management_system/nutrient_management_system/settings.py
+++ nutrient_management_system/nutrient_management_system/settings.py
@@ -27,8 +27,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = "chetanyashrimalie5@gmail.com"
-EMAIL_HOST_PASSWORD = 'cenanation'
+EMAIL_HOST_USER = "<Enter Email>"
+EMAIL_HOST_PASSWORD = '<Enter Password>'
 EMAIL_PORT = 587
 
 # Application definition
